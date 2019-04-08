72361b0cec1b6832719bb19212a8c7822a3845c2
Josh Kleinschmidt <lalaman744@hotmail.com>
2019-04-08 12:10:47 -0500

    Initial Commit

---
+++ Sunrise Sunset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunrise Sunset
+//
+//  Created by Josh Kleinschmidt on 4/8/19.
+//  Copyright © 2019 Josh Kleinschmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
